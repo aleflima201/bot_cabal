3777b52db8c7b4d33a435ae7c706a96c785bba3e
Aleph Lima <alef.lima201@gmail.com>
2025-02-03 20:09:24 -0300

    adicionado interface de usuario

--- interface_login.py
+++ interface_login.py
@@ -2,8 +2,47 @@ import customtkinter as ctk
 
 ctk.set_appearance_mode('dark')
 
+#função de validação de usuario
+def validar_login():
+    usuario = entry_usuario.get()
+    senha = entry_senha.get()
+    if usuario == "bk201" or senha == "12345":
+        label_valid.configure(text="Loguin realizado com sucesso!", text_color='green')
+    else:
+        label_valid.configure(text="Usuário ou Senha incorreto!", text_color='red')
+    
+
+
+#tela de login
 app = ctk.CTk()
 app.title('Sistema de login')
 app.geometry('300x300')
+
+#label texto do usuario
+label_usuario = ctk.CTkLabel(app, text="Digite o Usuario")
+label_usuario.pack(pady=10)
+
+#Entry texto do usuario
+entry_usuario = ctk.CTkEntry(app, placeholder_text="Digite o Usuario")
+entry_usuario.pack(pady=1)
+
+#label texto da senha
+label_senha = ctk.CTkLabel(app, text="Digite a Senha")
+label_senha.pack(pady=10)
+
+#Entry texto do usuario
+entry_senha = ctk.CTkEntry(app, placeholder_text="Digite a Senha", show='*')
+entry_senha.pack(pady=1)
+
+checkbox = ctk.CTkCheckBox(app, text="Lembrar Login")
+
+#botão de conferir login
+botao = ctk.CTkButton(app, text="Login", command=validar_login)
+botao.pack(pady=10)
+
+#label texto de validação do login
+label_valid = ctk.CTkLabel(app, text="")
+label_valid.pack(pady=10)
+
 
 app.mainloop()
